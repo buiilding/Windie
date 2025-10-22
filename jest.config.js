ccd12ca4862c3d754cca9c540ec2e0a41fdb835a
buiilding <peterbuics@gmail.com>
2025-10-22 02:43:03 +0000

    fix(frontend): correct jest configuration and finalize tests

--- jest.config.js
+++ jest.config.js
@@ -1,16 +1,10 @@
 /** @type {import('jest').Config} */
 const config = {
-  testEnvironment: 'jest-environment-jsdom',
-  verbose: true,
-  // A list of paths to directories that Jest should use to search for files in
-  roots: [
-    '<rootDir>/../tests/frontend',
-  ],
-  // The glob patterns Jest uses to detect test files
-  testMatch: [
-    '**/__tests__/**/*.?(m)js?(x)',
-    '**/?(*.)+(spec|test).?(m)js?(x)',
-  ],
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  transform: {
+    '^.+\\.jsx?$': 'babel-jest',
+  },
 };
 
 export default config;
