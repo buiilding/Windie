2fe196b2e06e57ead60cedbcc109b2b5bdbd3891
buiilding <peterbuics@gmail.com>
2026-02-02 20:07:54 -0500

    test(frontend): silence noisy jest output

--- jest.setup.js
+++ jest.setup.js
@@ -1,4 +1,25 @@
-import '@testing-library/jest-dom';
+require('@testing-library/jest-dom');
 
-// Mock scrollIntoView as it is not implemented in jsdom
-window.HTMLElement.prototype.scrollIntoView = jest.fn();
+const suppressedPrefixes = [
+  '[ConfigStorage]',
+  '[ToolExecutionService]',
+  '[Timing]',
+];
+
+const originalConsole = {
+  log: console.log,
+  warn: console.warn,
+  error: console.error,
+};
+
+const shouldSuppress = (args) =>
+  typeof args[0] === 'string' && suppressedPrefixes.some((prefix) => args[0].startsWith(prefix));
+
+['log', 'warn', 'error'].forEach((method) => {
+  jest.spyOn(console, method).mockImplementation((...args) => {
+    if (shouldSuppress(args)) {
+      return;
+    }
+    originalConsole[method](...args);
+  });
+});
