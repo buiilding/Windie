d9da4eac241a83bf4bd5ea8039a744a1b3099969
buiilding <peterbuics@gmail.com>
2026-02-23 22:41:17 -0500

    refactor(chatbox-styles): split response overlay stylesheet

--- src/renderer/app/ChatBoxResponseApp.jsx
+++ src/renderer/app/ChatBoxResponseApp.jsx
@@ -4,6 +4,7 @@ import { ChatProvider } from './providers/ChatProvider';
 import ChatBoxResponse from '../features/chat/components/ChatBoxResponse';
 import '../styles/theme.css';
 import '../styles/ChatBox.css';
+import '../styles/ChatBoxResponseOverlay.css';
 import '../styles/accessibility.css';
 
 function ChatBoxResponseApp() {
